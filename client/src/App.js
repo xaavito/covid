438235e9f8ee129bfaafc30c7a644510a64bc84e
Javier Martin Gonzalez <javiermartingonzalez@gmail.com>
2021-09-24 18:02:23 -0300

    Datepicker y actualizacion librerias

--- client/src/App.js
+++ client/src/App.js
@@ -8,66 +8,28 @@ import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-//import StarIcon from '@mui/icons-material/StarIcon';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Covid Bot
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
-
-const tiers = [
-  {
-    title: 'Free',
-    price: '0',
-    description: [
-      '10 users included',
-      '2 GB of storage',
-      'Help center access',
-      'Email support',
-    ],
-    buttonText: 'Sign up for free',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: 'Pro',
-    subheader: 'Most popular',
-    price: '15',
-    description: [
-      '20 users included',
-      '10 GB of storage',
-      'Help center access',
-      'Priority email support',
-    ],
-    buttonText: 'Get started',
-    buttonVariant: 'contained',
-  },
-  {
-    title: 'Enterprise',
-    price: '30',
-    description: [
-      '50 users included',
-      '30 GB of storage',
-      'Help center access',
-      'Phone & email support',
-    ],
-    buttonText: 'Contact us',
-    buttonVariant: 'outlined',
-  },
-];
-
+/*
 const footers = [
   {
     title: 'Company',
@@ -92,8 +54,8 @@ const footers = [
     description: ['Privacy policy', 'Terms of use'],
   },
 ];
-
-function PricingContent() {
+*/
+function App() {
   return (
     <React.Fragment>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
@@ -148,79 +110,93 @@ function PricingContent() {
           color="text.primary"
           gutterBottom
         >
-          Pricing
+          Covid-bot
         </Typography>
         <Typography variant="h5" align="center" color="text.secondary" component="p">
-          Quickly build an effective pricing table for your potential customers with
-          this layout. It&apos;s built with default MUI components with little
-          customization.
+          Aplicacion para la obtencion de resultados de COVID provistos por
+          http://datos.salud.gob.ar/dataset/covid-19-casos-registrados-en-la-republica-argentina
         </Typography>
       </Container>
       {/* End hero unit */}
       <Container maxWidth="md" component="main">
         <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={"Prueba"}
-              xs={12}
-              sm={12}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={"Prueba"}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  action={null}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
+          <Grid
+            item
+            key={"Prueba"}
+            xs={12}
+            sm={12}
+            md={6}
+          >
+            <Card>
+              <CardHeader
+                title={"Filtros"}
+                subheader={"Filtros"}
+                titleTypographyProps={{ align: 'center' }}
+                action={null}
+                subheaderTypographyProps={{
+                  align: 'center',
+                }}
+                sx={{
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                      ? theme.palette.grey[200]
+                      : theme.palette.grey[700],
+                }}
+              />
+              <CardContent>
+                <Box
                   sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'baseline',
+                    mb: 2,
                   }}
-                />
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
-                      mb: 2,
-                    }}
-                  >
-                    <Typography component="h2" variant="h3" color="text.primary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="text.secondary">
-                      /mo
-                    </Typography>
-                  </Box>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant}>
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+                >
+                  <DatePicker />
+                </Box>
+              </CardContent>
+              <CardActions>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            key={"Resultados"}
+            xs={12}
+            sm={12}
+            md={6}
+          >
+            <Card>
+              <CardHeader
+                title={"Prueba"}
+                subheader={"Prueba"}
+                titleTypographyProps={{ align: 'center' }}
+                action={null}
+                subheaderTypographyProps={{
+                  align: 'center',
+                }}
+                sx={{
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                      ? theme.palette.grey[200]
+                      : theme.palette.grey[700],
+                }}
+              />
+              <CardContent>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'baseline',
+                    mb: 2,
+                  }}
+                >
+                </Box>
+              </CardContent>
+              <CardActions>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </Container>
       {/* Footer */}
@@ -233,23 +209,8 @@ function PricingContent() {
           py: [3, 6],
         }}
       >
+
         <Grid container spacing={4} justifyContent="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="text.primary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="text.secondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
         </Grid>
         <Copyright sx={{ mt: 5 }} />
       </Container>
@@ -259,5 +220,5 @@ function PricingContent() {
 }
 
 export default function Pricing() {
-  return <PricingContent />;
+  return <App />;
 }
